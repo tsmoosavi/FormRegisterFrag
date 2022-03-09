e52690ebd57346b3770a5c370c9e786b4426e94a
ts moosavi <tt.musavi@gmail.com>
2022-03-09 12:54:03 +0330

    views named in second fragment

--- app/src/main/java/com/example/formregisterfrag/SecondeFragment.kt
+++ app/src/main/java/com/example/formregisterfrag/SecondeFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.google.android.material.button.MaterialButton
 
 
 class SecondeFragment : Fragment() {
@@ -22,5 +26,16 @@ class SecondeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_seconde, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initView()
+        super.onViewCreated(view, savedInstanceState)
+    }
+    private fun initView() {
+        var fullNameBtn = view?.findViewById<MaterialButton>(R.id.fullnamebox)
+        var userNameBtn = view?.findViewById<MaterialButton>(R.id.usernamebox)
+        var emailBtn = view?.findViewById<MaterialButton>(R.id.emailbox)
+        var passWordBtn = view?.findViewById<MaterialButton>(R.id.passwordbox)
+        var genderBtn = view?.findViewById<MaterialButton>(R.id.genderbox)
+        var saveBtn = view?.findViewById<MaterialButton>(R.id.savebox)
+    }
 }
