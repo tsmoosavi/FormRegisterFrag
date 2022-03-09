af0c40aa46066c350038ae7fce8aa439d086700f
ts moosavi <tt.musavi@gmail.com>
2022-03-09 12:22:14 +0330

    views named in first fragment

--- app/src/main/java/com/example/formregisterfrag/FirstFragment.kt
+++ app/src/main/java/com/example/formregisterfrag/FirstFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 
 class FirstFragment : Fragment() {
 
@@ -22,5 +26,22 @@ class FirstFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        var fullNameTxv = view?.findViewById<EditText>(R.id.FullName)
+        var userNameTxv = view?.findViewById<EditText>(R.id.userName)
+        var emailTxv = view?.findViewById<EditText>(R.id.email)
+        var passWordTxv = view?.findViewById<EditText>(R.id.password)
+        var reTypePassWordTxv = view?.findViewById<EditText>(R.id.reTypePassword)
+        var gender = when(view?.findViewById<RadioGroup>(R.id.genderRadio)?.checkedRadioButtonId){
+            view?.findViewById<RadioButton>(R.id.radioButton)?.id -> "زن"
+            view?.findViewById<RadioButton>(R.id.radioButton2)?.id -> "مرد"
+            else -> "not chosen yet"
+        }
+    }
 
 }
